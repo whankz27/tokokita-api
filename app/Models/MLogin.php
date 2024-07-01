0a3f775a54d55e4cf7e00c11faca3a61c51ab2be
Ramdhan Connelo <37245888+whankz27@users.noreply.github.com>
2024-07-02 04:54:59 +0700

    Add files via upload

---
+++ app/Models/MLogin.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use CodeIgniter\Model;
+
+class MLogin extends Model
+
+{
+    protected $table = 'member_token';
+    protected $allowedFields = ['member_id', 'auth_key'];
+}
